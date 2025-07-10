9986fa0f69c5531d3e60208153ae0ea3fb0fc717
Stanislav <ssovelich@gmail.com>
2025-07-10 14:12:52 +0300

    added styles for postItem

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
-import styles from './PostItem.module.css';
+import styles from '@/styles/components/PostItem.module.css';
+import { TiArrowForward } from "react-icons/ti";
 
 export default function PostItem({ post }) {
   return (
     <li className={styles.postItem}>
       <h3>{post.title}</h3>
-      <Link href={`/posts/${post.id}`}>Read more</Link>
+      <Link className={styles.link} href={`/posts/${post.id}`}>Read more&nbsp;<TiArrowForward /></Link>
     </li>
   );
 }
