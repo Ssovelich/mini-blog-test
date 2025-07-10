18e2f3ae4487c175234ec79775408fd4d39b49a1
Stanislav <ssovelich@gmail.com>
2025-07-10 16:02:02 +0300

    added styles for PostPage

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,6 +1,8 @@
-import { getPost, getPosts } from '@/lib/api';
-import { notFound } from 'next/navigation';
-import styles from '@/styles/pages/PostPage.module.css';
+import { getPost, getPosts } from "@/lib/api";
+import { notFound } from "next/navigation";
+import styles from "@/styles/pages/PostPage.module.css";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
 export async function generateStaticParams() {
   const posts = await getPosts();
@@ -12,7 +14,11 @@ export default async function PostPage({ params }) {
   if (!post) notFound();
 
   return (
-    <div className={styles.wrapper}>
+    <div className={`container ${styles.postPageContainer}`}>
+      <Link className={styles.link} href="/">
+        <FaArrowLeft />&nbsp;Go back
+      </Link>
+      <hr className={styles.divider} />
       <h1>{post.title}</h1>
       <p>{post.body}</p>
     </div>
