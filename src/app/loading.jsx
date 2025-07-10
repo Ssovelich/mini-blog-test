15cdc920cca7c8489ac6a63f2dc717a02ffad64d
Stanislav <ssovelich@gmail.com>
2025-07-10 15:20:05 +0300

    fix position loader

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,10 +1,12 @@
 import styles from "@/styles/components/Loading.module.css";
 
 const Loader = () => (
-  <div className={styles.loader}>
-    <div className={styles.circle} />
-    <div className={styles.circle} />
-    <div className={styles.circle} />
+  <div className={styles.loaderWrapper}>
+    <div className={styles.loader}>
+      <div className={styles.circle} />
+      <div className={styles.circle} />
+      <div className={styles.circle} />
+    </div>
   </div>
 );
 
